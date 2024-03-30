dcba6693e16d9d88d21ad38e85efd57ae0758721
Adrián San Martín <sanmartin.adrian@gmail.com>
2024-03-30 12:47:25 -0300

    add integration auth-astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+import auth from "auth-astro";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  integrations: [auth()]
+});
